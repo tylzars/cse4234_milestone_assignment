824a04ac5f4e8da32a883f068cd835b29970b072
Tyler Zars <tyler@zars.me>
2023-04-19 17:38:38 -0400

    Working Email/Password Auth using AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { useContext, createContext, useEffect, useState } from 'react';
-import { GoogleAuthProvider, signOut, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, signOut, onAuthStateChanged, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 //import { GoogleAuthProvider, signInWithRedirect, signOut, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
 import { auth } from '../firebase_config';
 
@@ -12,6 +12,25 @@ export const AuthContextProvider = ({ children }) => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
     //signInWithRedirect(auth, provider)
+  };
+
+  const handleSignIn = async (e, email, password) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSignUp = async (e, email, password) => {
+    e.preventDefault();
+    console.log(email+":"+password)
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const logOut = () => {
@@ -29,7 +48,7 @@ export const AuthContextProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ googleSignIn, logOut, user }}>
+    <AuthContext.Provider value={{ googleSignIn, logOut, user, handleSignUp, handleSignIn }}>
       {children}
     </AuthContext.Provider>
   );
--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,6 +1,6 @@
+import { useState } from 'react';
 import { UserAuth } from '../context/AuthContext';
 import { GoogleButton } from 'react-google-button';
-
 
 import NavBar from './components/navbar'
 import Header from './components/header'
@@ -8,7 +8,9 @@ import Footer from './components/footer'
 
 
 function Login() {
-  const { googleSignIn, user } = UserAuth();
+  const { handleSignIn, handleSignUp, googleSignIn, user } = UserAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleGoogleSignIn = async () => {
     try {
@@ -18,10 +20,65 @@ function Login() {
     }
   };
 
+  const handleUserSignIn = async (e) => {
+    try {
+      await handleSignIn(e, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUserSignUp = async (e) => {
+    try {
+      await handleSignUp(e, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
   return (
     <>
         <Header />
         <NavBar />
+
+
+        <form onSubmit={handleUserSignIn}>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+          />
+          <button type="submit">Sign In</button>
+        </form>
+
+        <form onSubmit={handleUserSignUp}>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+          />
+          <button type="submit">Sign Up</button>
+        </form>
+
         <div>
             <h1 className='text-center text-3xl font-bold py-8'>Sign in</h1>
             <div className='max-w-[240px] m-auto py-4'>
