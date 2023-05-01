b62dd3a46f944cb41fd1d4ee38312869c7f90eed
Tyler Zars <tyler@zars.me>
2023-05-01 15:11:07 -0400

    Connect to AtlasMongo Cluster

--- src/backend-express/src/index.js
+++ src/backend-express/src/index.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 
 // npm install express mongodb
@@ -6,6 +6,30 @@ const express = require('express');
 // Setup App
 const app = express();
 app.use(express.json());
+
+// MongoDB Connection
+const uri = "mongodb+srv://tzars2019:iHdsXCBX62dNSUOg@cse4234-milestone-clust.ysexvq6.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 // Build an endpoint
 app.get('/', (req, res) => {
