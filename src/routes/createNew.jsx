5ed216e0eb5a7f3f22a75329587041c59fe5764a
Tyler Zars <tyler@zars.me>
2023-05-01 15:33:33 -0400

    Convert createNew to React for parameter passing

--- src/routes/createNew.jsx
+++ src/routes/createNew.jsx
@@ -1,8 +1,30 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/header'
 import Footer from './components/footer'
 
 
-function createNew() {
+const CreateNew = () => {
+    const [taskName, setTaskName] = useState("");
+    const [taskCategory, setTaskCategory] = useState("");
+    const [taskDueDate, setTaskDueDate] = useState("");
+    const [taskUrgency, setTaskUrgency] = useState("");
+    const [taskOtherNotes, setTaskOtherNotes] = useState("");
+
+  
+    const createNewTask = async () =>{
+      // Send Data to our express route
+      console.log(taskName, taskCategory, taskDueDate, taskUrgency, taskOtherNotes)
+
+      // Reset all fields since old task is created
+      setTaskName("");
+      setTaskCategory("");
+      setTaskDueDate("");
+      setTaskUrgency("");
+      setTaskOtherNotes("")
+    }
+
+
     return (
         <>
             <meta charSet="utf-8" />
@@ -15,31 +37,38 @@ function createNew() {
                 <section>
                 <h1>Welcome to createNew.html!</h1>
                 </section>
-                <form className="createNewListItem" action="./list.html" method="get">
-                <label htmlFor="task_name">Task Name:</label>
-                <input type="text" id="task_name" name="task_name" />
-                <br />
-                <label htmlFor="task_category">Task Category:</label>
-                <input type="text" id="task_category" name="task_category" />
-                <br />
-                <label htmlFor="todo_date">Todo Date:</label>
-                <input type="date" id="todo_date" name="todo_date" />
-                <br />
-                <label htmlFor="task_urgency">Task Urgency:</label>
-                <input type="text" id="task_urgency" name="task_urgency" />
-                <br />
-                <br />
-                <label htmlFor="textarea">Other Task Notes</label>
-                <br />
-                <textarea
-                    id="textarea"
-                    name="textarea"
-                    rows={7}
-                    cols={80}
-                    defaultValue={""}
-                />
-                <br />
-                <input type="submit" defaultValue="Submit" />
+                <form className="createNewListItem">
+                    <label htmlFor="task_name">Task Name:</label>
+                    <input type="text" id="task_name" name="task_name" value = {taskName} onChange={e => setTaskName(e.target.value)} required />
+                    <br />
+
+                    <label htmlFor="task_category">Task Category:</label>
+                    <input type="text" id="task_category" name="task_category" value={taskCategory} onChange={e => setTaskCategory(e.target.value)} required />
+                    <br />
+
+                    <label htmlFor="todo_date">Todo Date:</label>
+                    <input type="date" id="todo_date" name="todo_date" value={taskDueDate} onChange={e => setTaskDueDate(e.target.value)} required />
+                    <br />
+
+                    <label htmlFor="task_urgency">Task Urgency:</label>
+                    <input type="text" id="task_urgency" name="task_urgency" value={taskUrgency} onChange={e => setTaskUrgency(e.target.value)} required />
+                    <br />
+
+                    <br />
+                    <label htmlFor="textarea">Other Task Notes</label>
+                    <br />
+
+                    <textarea
+                        id="textarea"
+                        name="textarea"
+                        rows={7}
+                        cols={80}
+                        value={taskOtherNotes}
+                        onChange={e => setTaskOtherNotes(e.target.value)}
+                    />
+                    <br />
+
+                    <input type="submit" defaultValue="Submit" onClick={createNewTask}/>
                 </form>
             </article>
             <div className="bottom">
@@ -53,4 +82,4 @@ function createNew() {
     )
 }
 
-export default createNew;
+export default CreateNew;
