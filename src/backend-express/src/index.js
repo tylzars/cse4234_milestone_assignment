a96cfd70b247d9eba86d8d8eb6fa3652b474a3af
Tyler Zars <tyler@zars.me>
2023-05-01 18:55:27 -0400

    Enable cors, holy moly it works now

--- src/backend-express/src/index.js
+++ src/backend-express/src/index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient } = require("mongodb");
 const express = require("express");
 const cors = require('cors')
 
@@ -7,38 +7,17 @@ const cors = require('cors')
 // Setup App
 const app = express();
 app.use(express.json());
-app.use(cors)
 
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:4000'];
 const corsOptions = {
-    origin: 'http://*'
-};
+    origin: allowedOrigins,
+    optionsSuccessStatus: 200
+}
 app.use(cors(corsOptions));
+  
 
 // MongoDB Connection
 const uri = "mongodb+srv://tzars2019:iHdsXCBX62dNSUOg@cse4234-milestone-clust.ysexvq6.mongodb.net/?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 // Build an endpoint
 app.get("/", (req, res) => {
