d0e5c0c682266dcf03dcb82875b046569db45484
Tyler Zars <tyler@zars.me>
2023-05-02 11:34:57 -0400

    Example task in detail until data is loaded, route  /detail works with no id

--- src/routes/detail.jsx
+++ src/routes/detail.jsx
@@ -7,41 +7,47 @@ const Detail = () => {
     const { user } = UserAuth();
 
     const [userDetailTask, setUserDetailTask] = useState(null); 
+    const [defaultPage, setDefaultPage] = useState(true)
 
 
     useEffect(() => {
         const loadData = async () => {
             // Load task from api via our passed _id
             const current_url = window.location.href
-            const custom_url = 'https://cse4234-milestone-node.onrender.com/api/task/' + current_url.split('/').pop()
+            if (current_url.split('/').pop() === "") {
+                setDefaultPage(true)
+            } else {
+                const custom_url = 'https://cse4234-milestone-node.onrender.com/api/task/' + current_url.split('/').pop()
 
-            await fetch(custom_url, { 
-                method: 'GET', 
-                mode: 'cors', 
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            })
-            .then(response => {
-                if (!response.ok) {
-                throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Success:', data);
-                // Get back the data
-
-                if(data.length > 0){ 
-                    const temp_tasks = data;
-                    setUserDetailTask(temp_tasks);
-                } else {
-                    console.log("didn't update")
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+                await fetch(custom_url, { 
+                    method: 'GET', 
+                    mode: 'cors', 
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                })
+                .then(response => {
+                    if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log('Success:', data);
+                    // Get back the data
+    
+                    if(data.length > 0){ 
+                        const temp_tasks = data;
+                        setUserDetailTask(temp_tasks);
+                    } else {
+                        console.log("didn't update")
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+                setDefaultPage(false)
+            }
         }
         loadData();
     }, [user.uid]);
@@ -76,6 +82,32 @@ const Detail = () => {
         )
     }
 
+    function displayExampleDetailTask() {
+        return(
+            <div>
+                <h2>Welcome to detail.html!</h2>
+                <h3>Example To Do List</h3>
+                <ul>
+                    <li>
+                        <div>
+                            <h4>Task: Example Task #1 </h4>
+                            <p>Topic: Category #1</p>
+                            <p>Urgency: High</p>
+                            <p>Due: Month/Day/Year</p>
+                            <p>Other Notes: Long Text Here</p>
+                        </div>
+                    </li>
+                </ul>
+                <button onClick={() => window.location.href='/list'}>
+                    Add updated task to list!
+                </button>
+                <button onClick={() => window.location.href='/list'}>
+                    Return to list.html
+                </button>
+            </div>
+        )
+    }
+
     return (
         <>
             {/* Store Start & Navbar */}
@@ -83,7 +115,8 @@ const Detail = () => {
 
             <article>
                 <section>
-                    {userDetailTask && displayDetailTask()}
+                    {!defaultPage && displayDetailTask()}
+                    {defaultPage && displayExampleDetailTask()}
                 </section>
             </article>
 
