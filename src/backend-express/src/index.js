6d672967b93c4c750d64f7f461a3ea474d1b2f0d
Tyler Zars <tyler@zars.me>
2023-05-01 21:47:04 -0400

    Remove netlify stuff

--- src/backend-express/src/index.js
+++ src/backend-express/src/index.js
@@ -1,7 +1,6 @@
 const { MongoClient } = require("mongodb");
 const express = require("express");
 const cors = require('cors')
-const serverless = require('serverless-http');
 
 
 // npm install express mongodb cors
@@ -17,20 +16,18 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-const router = express.Router();
-
   
 
 // MongoDB Connection
 const uri = "mongodb+srv://tzars2019:iHdsXCBX62dNSUOg@cse4234-milestone-clust.ysexvq6.mongodb.net/?retryWrites=true&w=majority";
 
 // Build an endpoint
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send([{ title: "API Output" }]);
 });
 
 // Add CreateNew Endpoint
-router.post('/api/createnew/', async (req, res) => {
+app.post('/api/createnew/', async (req, res) => {
     // Get data from request
     const { uid, taskName, taskCategory, taskDueDate, taskUrgency, taskOtherNotes } = req.body;
     console.log(uid, taskName)
@@ -58,7 +55,7 @@ router.post('/api/createnew/', async (req, res) => {
 
 // Get tasks endpoint
 // manual curl with: curl -d '{"uid":"jHXzqrZxL8h4D4eThXPOgZjVNmw1"}' -H "Content-Type: application/json" -X POST localhost:4000/api/tasks
-router.get('/api/tasks/:uid', async (req, res) => {
+app.get('/api/tasks/:uid', async (req, res) => {
     const uid = req.params.uid;
   
     // Setup mongo connection
@@ -78,14 +75,9 @@ router.get('/api/tasks/:uid', async (req, res) => {
       res.sendStatus(404);
     }
   });
-  
-app.use('/.netlify/functions/server', router);
 
 app.listen(4000, () => {
   console.log("Listening on port 4000");
 });
-
-module.exports = app;
-module.exports.handler = serverless(app);
 
 //console.log("fun")
